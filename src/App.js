05a28fafdb0ef832748010411b6a9770b46649b1
RohithGowda321 <160516020+RohithGowda321@users.noreply.github.com>
2024-07-01 12:27:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {process.env.REACT_APP_ENV === 'production' ? (
+        <>
+          <meta name="description" content="Production description" />
+          <meta property="og:title" content="Production Title" />
+        <meta property="og:image" content="https://coffeeweb.s3.ap-south-1.amazonaws.com/coffeeweb_menu_icons/CoffeeWeb_Logo_White_Background_Blue_Text.png" />
+
+          {/* Add other production meta tags */}
+        </>
+      ) : (
+        <>
+          <meta name="description" content="Development description" />
+          <meta property="og:title" content="Development Title" />
+          {/* Add other development meta tags */}
+        </>
+      )}
+      <noscript>You need to enable JavaScript to run this app.</noscript>
+      <div id="root"></div>
     </div>
   );
 }
