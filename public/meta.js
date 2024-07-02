d613b2ae301a61ec55a65df6753c54199f7f9ff3
RohithGowda321 <160516020+RohithGowda321@users.noreply.github.com>
2024-07-02 09:48:50 +0530

    Add files via upload

---
+++ public/meta.js
@@ -0,0 +1,17 @@
+// meta.js
+const metaTags = [
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { name: "description", content: "This app provides end-to-end information about the Global Coffee Industry" },
+    { property: "og:title", content: "www.coffeeweb.com" },
+    { property: "og:description", content: "This app provides end-to-end information about the Global Coffee Industry" },
+    { property: "og:url", content: "www.coffeeweb.com" },
+    { property: "og:type", content: "website" },
+    { property: "og:image", content: "https://coffeeweb.s3.ap-south-1.amazonaws.com/coffeeweb_menu_icons/CoffeeWeb_Logo_White_Background_Blue_Text.png" }
+  ];
+  
+  metaTags.forEach(tag => {
+    const meta = document.createElement('meta');
+    Object.keys(tag).forEach(key => meta.setAttribute(key, tag[key]));
+    document.head.appendChild(meta);
+  });
+  
